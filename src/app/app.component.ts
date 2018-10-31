4cf0694feedfd4ab68e57024c0f793cfda2309f1
daliyajohn <daliyajohn75@gmail.com>
2018-10-31 13:36:27 +0530

    Layout fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 'Manager', name: 'Daliya' },
+  { position: 'Analyst', name: 'Seeniya' },
+  { position: 'Developer', name: 'Viji' },
+  { position: 'Tester', name: 'John' },
+  { position: 'Designer', name: 'Ryan' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +20,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular7-CRUD';
+  displayedColumns: string[] = ['Name', 'Position', 'Action'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
